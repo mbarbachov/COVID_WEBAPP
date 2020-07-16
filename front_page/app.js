189b1ab76e7e209c49556612cbba52200dd4f1c6
Michael <michael@grigoris-mbp.attlocal.net>
2020-07-16 14:06:12 -0700

    commit

--- front_page/app.js
+++ front_page/app.js
@@ -1,48 +1,48 @@
-function createMap() {
-    let map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 8,
-        center: { lat: 40.731, lng: -73.997 }
-    });
-
-    let marker = new google.maps.Marker({
-        position: { lat: 40.731, lng: -73.997 },
-        map: map
-    });
-    let geocoder = new google.maps.Geocoder();
-
-    map.addListener("click", (e) => {
-        marker = placeMarkerAndPanTo(marker, e.latLng, map)
-        let result = geocodeLatLng(geocoder, map, marker);
-
-        // open model after alert is resolved
-        openModal(myModal);
-    });
-}
-
-function placeMarkerAndPanTo(prevMarker, latLng, map) {
-    prevMarker.setMap(null);
-
-    // pan to the location and create a marker
-    map.panTo(latLng);
-    return new google.maps.Marker({
-        position: latLng,
-        map: map
-    });
-}
-
-function geocodeLatLng(geocoder, map, marker) {
-    geocoder.geocode({ location: marker.latLng }, function(results, status) {
-        if (status === "OK") {
-            if (results[0]) {
-                window.alert(results);
-            } else {
-                window.alert("No results found.");
-            }
-        } else {
-            window.alert("Geocoder failed due to: " + status);
-        }
-    });
-}
+// function createMap() {
+//     let map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 8,
+//         center: { lat: 40.731, lng: -73.997 }
+//     });
+//
+//     let marker = new google.maps.Marker({
+//         position: { lat: 40.731, lng: -73.997 },
+//         map: map
+//     });
+//     let geocoder = new google.maps.Geocoder();
+//
+//     map.addListener("click", (e) => {
+//         marker = placeMarkerAndPanTo(marker, e.latLng, map)
+//         let result = geocodeLatLng(geocoder, map, marker);
+//
+//         // open model after alert is resolved
+//         openModal(myModal);
+//     });
+// }
+//
+// function placeMarkerAndPanTo(prevMarker, latLng, map) {
+//     prevMarker.setMap(null);
+//
+//     // pan to the location and create a marker
+//     map.panTo(latLng);
+//     return new google.maps.Marker({
+//         position: latLng,
+//         map: map
+//     });
+// }
+//
+// function geocodeLatLng(geocoder, map, marker) {
+//     geocoder.geocode({ location: marker.latLng }, function(results, status) {
+//         if (status === "OK") {
+//             if (results[0]) {
+//                 window.alert(results);
+//             } else {
+//                 window.alert("No results found.");
+//             }
+//         } else {
+//             window.alert("Geocoder failed due to: " + status);
+//         }
+//     });
+// }
 
 // modal displays and overlays
 let modal = document.getElementById('simpleModal');
